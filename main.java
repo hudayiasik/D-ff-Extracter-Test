0a84b339dd8a17fd58f31306b0764cda2697b303
hudayiasik <hudayi.asik@stu.fsm.edu.tr>
2024-03-07 23:41:19 +0300

    factorial calculate and diff files added

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
 public class main{
     public static void main(String[] args) {
+
+
         System.out.println("Hi World!");
+        int num1 = 0;
+        factorial(num1);
+
+    }
+    static void factorial(int num){
+        int fact = 1;
+        for(int i = 2; i <= num;i++){
+            fact *= i;
+        }
+        System.out.println("The factorial of " + num + " is " + fact);
     }
 }
